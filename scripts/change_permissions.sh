6040054ae778bccd50a8c57471ad5a1983048687
tuaph <tuaph@amazon.com>
2021-04-28 17:07:57 +1000

    install and configure tomcat server

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+if [[ ! -d /var/www/html ]]; then
+    mkdir -p /var/www/html
+fi
 chmod -R 777 /var/www/html
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+amazon-linux-extras install java-openjdk11 -y
+groupadd --system tomcat
+useradd -d /usr/share/tomcat -r -s /bin/false -g tomcat tomcat
+export VER="9.0.41"
+wget https://archive.apache.org/dist/tomcat/tomcat-9/v${VER}/bin/apache-tomcat-${VER}.tar.gz
+tar xvf apache-tomcat-${VER}.tar.gz -C /usr/share/
+ln -s /usr/share/apache-tomcat-$VER/ /usr/share/tomcat
+chown -R tomcat:tomcat /usr/share/tomcat
+chown -R tomcat:tomcat /usr/share/apache-tomcat-$VER/ 
+tee /etc/systemd/system/tomcat.service<<EOF
+[Unit]
+Description=Tomcat Server
+After=syslog.target network.target
+
+[Service]
+Type=forking
+User=tomcat
+Group=tomcat
+
+Environment=JAVA_HOME=/usr/lib/jvm/jre
+Environment='JAVA_OPTS=-Djava.awt.headless=true'
+Environment=CATALINA_HOME=/usr/share/tomcat
+Environment=CATALINA_BASE=/usr/share/tomcat
+Environment=CATALINA_PID=/usr/share/tomcat/temp/tomcat.pid
+Environment='CATALINA_OPTS=-Xms512M -Xmx1024M'
+ExecStart=/usr/share/tomcat/bin/catalina.sh start
+ExecStop=/usr/share/tomcat/bin/catalina.sh stop
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl start tomcat
+systemctl enable tomcat
